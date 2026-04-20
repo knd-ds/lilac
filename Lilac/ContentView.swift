bfc73fef3f80afca83fa34da304645d5034d976b
Candela Cabido <candelacabido@gmail.com>
2026-04-20 06:22:23 -0300

    Initial Commit

---
+++ Lilac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lilac
+//
+//  Created by Candela Cabido on 20/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lilac/LilacApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilacApp.swift
+//  Lilac
+//
+//  Created by Candela Cabido on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct LilacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
